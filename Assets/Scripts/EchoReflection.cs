9cd0195fbc1d5c6b2298562d055c0bcb93a79198
nadnbuds <lalam003@ucr.edu>
2017-01-21 04:32:18 -0800

    Added Echo reflection object
    Needs some more work and fine tuning with physics

---
+++ Assets/Scripts/EchoReflection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EchoReflection : MonoBehaviour {
+
+    public Echo prefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // Needs to check tag to see if it was an echo that hit
+        print("reflect");
+        Echo reflect = Instantiate(prefab, transform.position, Quaternion.identity);
+        //Currently reflects 180, need to implement proper physics reflecting
+        reflect.transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+    }
+}
