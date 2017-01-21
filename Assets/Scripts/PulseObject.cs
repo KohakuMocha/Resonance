f355dc5caed0321a2237cfe414d25df1bfaabf3a
Jeffrey Hsieh <jhsie007@ucr.edu>
2017-01-21 05:43:14 -0800

    Added Pulse.

---
+++ Assets/Scripts/PulseObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseObject : MonoBehaviour {
+
+	public GameObject wave;
+	public int pulse;
+	public float time;
+
+	void PulseWave(){
+		for (int i = 0; i < pulse; i++) {
+			Instantiate (wave, gameObject.transform.position, Quaternion.identity);
+		}
+	}
+
+	void Start() {
+		InvokeRepeating("PulseWave", 0, 2.0f);
+	}
+}
