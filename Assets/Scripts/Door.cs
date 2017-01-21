a9bfcab57fb6a2b8703b59350cd5ef067fda7bf2
nadnbuds <lalam003@ucr.edu>
2017-01-21 13:36:43 -0800

    Added Door/Switch and smooth boulder animation

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public List<int> SwitchKeyValues;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void checkOpened()
+    {
+        if (SwitchKeyValues.Count == 0)
+        { 
+            Destroy(this.GetComponent<BoxCollider2D>());
+
+        }
+    }
+}
