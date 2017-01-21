6004ad1e3a1ca046ec8651e7d4e9d5b7f48ae59f
nadnbuds <lalam003@ucr.edu>
2017-01-21 01:32:34 -0800

    Added Boulder Code

---
+++ Assets/Scripts/MoveableObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableObject : MonoBehaviour {
+    public float Scale;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Vector3 position = collision.transform.position;
+         position -= this.transform.position;
+         Vector3 offset = new Vector3(0.1f, 0, 0);
+         if(Mathf.Abs(position.x) >= Mathf.Abs(position.y))
+         {
+             if(position.x > 0)
+             {
+                //Create Coroutine to slowly transform position to give animation
+                GetComponent<Rigidbody2D>().MovePosition(transform.position - transform.right * .01f);
+            }
+             else
+             {
+                GetComponent<Rigidbody2D>().MovePosition(transform.position + transform.right * .01f);
+            }
+         }
+         else
+         {
+             if (position.y > 0)
+             {
+                GetComponent<Rigidbody2D>().MovePosition(transform.position - transform.up * .01f);
+            }
+             else
+             {
+                GetComponent<Rigidbody2D>().MovePosition(transform.position + transform.up * .01f);
+            }
+         }
+    }
+
+}
