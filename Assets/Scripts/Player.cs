71effde580e642d4de26aeec9ffc7e981bc0306a
dzhan008 <dzhan008@ucr.edu>
2017-01-20 22:48:24 -0800

    Added Movement/Singleton Class
    Added basic movement to the player.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Singleton<Player> {
+
+    public int speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton("Up"))
+        {
+            GetComponent<Rigidbody2D>().MovePosition(transform.position + transform.up * speed * Time.deltaTime);
+        }
+        else if (Input.GetButton("Down"))
+        {
+            GetComponent<Rigidbody2D>().MovePosition(transform.position + -transform.up * speed * Time.deltaTime);
+        }
+        else if (Input.GetButton("Left"))
+        {
+            GetComponent<Rigidbody2D>().MovePosition(transform.position + -transform.right * speed * Time.deltaTime);
+        }
+        else if (Input.GetButton("Right"))
+        {
+            GetComponent<Rigidbody2D>().MovePosition(transform.position + transform.right * speed * Time.deltaTime);
+        }
+	}
+}
