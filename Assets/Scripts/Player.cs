6078b0f4a5e18419455fe598962b8583bdfc1f3d
Jeffrey Hsieh <jhsie007@ucr.edu>
2017-01-21 21:08:31 -0800

    GM

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Player : Singleton<Player>
 {
-
+	public Resonance waves;
     public int speed;
     public Vector3 savePosition;
     public GameObject echo;
     private float velocity = 3.0f;
     private Vector3 MousePos;
     private List<GameObject> Echoes = new List<GameObject>();
+
+	IEnumerator Wait(){
+		yield return new WaitForSeconds (2);
+	}
+
     // Use this for initialization
     void Start()
     {
@@ -23,32 +28,42 @@ public class Player : Singleton<Player>
 
         UpdateMouse();
 
-        // EKKO
-        if (Input.GetButtonDown("Space"))
-        {
-            // Create new wave.
-            GameObject wave = (GameObject)Instantiate(echo, transform.position, Quaternion.identity);
-            // Get player face rotation and set to object.
-            wave.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, gameObject.transform.rotation.z), 20 * Time.deltaTime);
-            Echoes.Add(wave);
-        }
-        if (Input.GetButton("Up"))
-        {
-            GetComponent<Rigidbody2D>().MovePosition(transform.position + transform.up * speed * Time.deltaTime);
-        }
-        else if (Input.GetButton("Down"))
-        {
-            GetComponent<Rigidbody2D>().MovePosition(transform.position + -transform.up * speed * Time.deltaTime);
-        }
-        else if (Input.GetButton("Left"))
-        {
-            GetComponent<Rigidbody2D>().MovePosition(transform.position + -transform.right * speed * Time.deltaTime);
-        }
-        else if (Input.GetButton("Right"))
-        {
-            GetComponent<Rigidbody2D>().MovePosition(transform.position + transform.right * speed * Time.deltaTime);
-        }
 
+		switch(waves){
+		case Resonance.Movement:
+			// ECHO
+			if (Input.GetButtonDown("Space"))
+			{
+				waves = Resonance.Echoing;
+				break;
+			}
+			if (Input.GetButton("Up"))
+			{
+				GetComponent<Rigidbody2D>().MovePosition(transform.position + transform.up * speed * Time.deltaTime);
+			}
+			else if (Input.GetButton("Down"))
+			{
+				GetComponent<Rigidbody2D>().MovePosition(transform.position + -transform.up * speed * Time.deltaTime);
+			}
+			else if (Input.GetButton("Left"))
+			{
+				GetComponent<Rigidbody2D>().MovePosition(transform.position + -transform.right * speed * Time.deltaTime);
+			}
+			else if (Input.GetButton("Right"))
+			{
+				GetComponent<Rigidbody2D>().MovePosition(transform.position + transform.right * speed * Time.deltaTime);
+			}
+			break;
+		case Resonance.Echoing:
+			// Create new wave.
+			GameObject wave = (GameObject)Instantiate (echo, transform.position, Quaternion.identity);
+			// Get player face rotation and set to object.
+			wave.transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0, 0, gameObject.transform.rotation.z), 20 * Time.deltaTime);
+			Echoes.Add (wave);
+			waves = Resonance.Movement;
+			StartCoroutine (Wait ());
+			break;
+		}
         for (int i = 0; i < Echoes.Count; i++)
         {
 			if (Echoes[i]) {
